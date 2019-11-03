d6e51cb20a7f3c14e3c265d84d6ef32984971025
Sam Vervaeck <vervaeck.sam@skynet.be>
2019-11-03 22:20:26 +0100

    Add some tests for the parser

---
+++ tests/test_parser.py
@@ -0,0 +1,62 @@
+
+from templately.scanner import Scanner
+from templately.parser import Parser
+from templately.ast import *
+
+import unittest
+
+class TestParser(unittest.TestCase):
+
+    def test_simple_add(self):
+        sc = Scanner('#<simple_add>', 'a + b', True)
+        p = Parser(sc)
+        e = p.parse_expression()
+        self.assertIsInstance(e, AppExpression)
+        self.assertEqual(len(e.operands), 2)
+        self.assertIsInstance(e.operands[0], VarRefExpression)
+        self.assertEqual(e.operands[0].name, 'a')
+        self.assertIsInstance(e.operands[1], VarRefExpression)
+        self.assertEqual(e.operands[1].name, 'b')
+
+    def test_nested_add(self):
+        sc = Scanner('#<simple_add>', '(a + b) + c', True)
+        p = Parser(sc)
+        e = p.parse_expression()
+        self.assertIsInstance(e, AppExpression)
+        self.assertEqual(len(e.operands), 2)
+        arg1 = e.operands[0]
+        self.assertIsInstance(arg1, AppExpression)
+        self.assertEqual(arg1.operands[0].name, 'a')
+        self.assertEqual(arg1.operands[1].name, 'b')
+        arg2 = e.operands[1]
+        self.assertIsInstance(arg2, VarRefExpression)
+        self.assertEqual(arg2.name, 'c')
+
+    def test_binary_operator_precedence1(self):
+        sc = Scanner('#<simple_add>', 'a * b + c', True)
+        p = Parser(sc)
+        e = p.parse_expression()
+        self.assertIsInstance(e, AppExpression)
+        self.assertEqual(len(e.operands), 2)
+        arg1 = e.operands[0]
+        self.assertIsInstance(arg1, AppExpression)
+        self.assertEqual(arg1.operands[0].name, 'a')
+        self.assertEqual(arg1.operands[1].name, 'b')
+        arg2 = e.operands[1]
+        self.assertIsInstance(arg2, VarRefExpression)
+        self.assertEqual(arg2.name, 'c')
+
+    def test_binary_operator_precedence1(self):
+        sc = Scanner('#<simple_add>', 'a + b * c', True)
+        p = Parser(sc)
+        e = p.parse_expression()
+        self.assertIsInstance(e, AppExpression)
+        self.assertEqual(len(e.operands), 2)
+        arg1 = e.operands[0]
+        self.assertIsInstance(arg1, VarRefExpression)
+        self.assertEqual(arg1.name, 'a')
+        arg2 = e.operands[1]
+        self.assertIsInstance(arg2, AppExpression)
+        self.assertEqual(arg2.operands[0].name, 'b')
+        self.assertEqual(arg2.operands[1].name, 'c')
+
