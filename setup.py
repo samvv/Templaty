3fc48d3ed49372708e505697e64ba06e23facf08
Sam Vervaeck <vervaeck.sam@skynet.be>
2019-11-03 12:11:57 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,46 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='tecoge',
+    version='1.0.0',
+    description='Generate programming code from template files',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/samvv/tecoge',
+    author='Sam Vervaeck',
+    author_email='vervaeck.sam@skynet.be',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Code Generators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='template code-generator c++ c php java python',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    python_requires='>=3.5, <4',
+    install_requires=[],
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+    entry_points={
+        'console_scripts': [
+            'tecoge=tecoge:main',
+        ],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/samvv/tecoge/issues',
+        'Source': 'https://github.com/samvv/tecoge/',
+    },
+)
