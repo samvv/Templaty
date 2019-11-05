6a9fd250bc11b917b3dcbf5d771fae23238ec22f
Sam Vervaeck <vervaeck.sam@skynet.be>
2019-11-05 20:17:31 +0100

    Fix indentation logic in evaluator

--- templaty/evaluator.py
+++ templaty/evaluator.py
@@ -31,7 +31,7 @@ def remove_first_newline(text):
             i += 1
             break
         if not (ch == ' ' or ch == '\t'):
-            break
+            return
         i += 1
     return text[i:]
 
@@ -43,7 +43,7 @@ def remove_last_newline(text):
             i -= 1
             break
         if not (ch == ' ' or ch == '\t'):
-            break
+            return
         i -= 1
     return text[0:i+1]
 
@@ -63,7 +63,7 @@ def get_indentation(text):
                 curr_indent += 1
             else:
                 has_chars = True
-    return min_indent
+    return min_indent if not math.isinf(min_indent) else curr_indent
 
 class Env:
 
@@ -115,6 +115,19 @@ def has_class_property(cls, name):
     except AttributeError:
         return False
 
+def strip_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    else:
+        return text
+
+def count_newlines(text):
+    count = 0
+    for ch in text:
+        if ch == '\n':
+            count += 1
+    return count
+
 EXCLUDE_BUILTINS = [FunctionBuiltin, VariableBuiltin]
 
 def get_all_builtins():
@@ -126,10 +139,17 @@ def get_all_builtins():
         for builtin_class in my_class.__subclasses__():
             to_visit.append(builtin_class)
 
-def evaluate(ast, data={}, indentation='  '):
+def evaluate(ast, data={}, indentation='  ', filename="#<anonymous>"):
+
+    if isinstance(ast, str):
+        from .scanner import Scanner
+        from .parser import Parser
+        sc = Scanner(filename, ast)
+        p = Parser(sc)
+        ast = p.parse_all()
 
     indent_override = 0
-    curr_indent = 0
+    curr_indent = ''
     at_blank_line = True
 
     def eval_code_expr(e, env):
@@ -154,26 +174,56 @@ def evaluate(ast, data={}, indentation='  '):
         out = ''
         for stmt in stmts:
             result = eval_statement(stmt, env)
-            if not isinstance(stmt, TextStatement):
-                result = remove_last_newline(result)
+            #  if not isinstance(stmt, TextStatement) and is_wrapped(stmt):
+            #      result = remove_last_newline(result)
             out += result
         return out
+
+    def eval_repeat(pattern, rng, body, sep=''):
+        env2 = env.fork()
+        out = ''
+        block_indent = curr_indent
+        is_wrapped = len(body) > 0 \
+           and isinstance(body[0], TextStatement) \
+           and starts_with_newline(body[0].text) \
+           and isinstance(body[-1], TextStatement) \
+           and ends_with_newline(body[-1].text)
+        for i in rng:
+            if i > 0: out += sep
+            env2.set(pattern.name, i)
+            children = list(body)
+            temp_out = ''
+            smallest_indent = math.inf
+            for j, child in enumerate(children):
+                result = eval_statement(child, env2)
+                smallest_indent = min(smallest_indent, get_indentation(result))
+                if j == 0 and is_wrapped: result = remove_first_newline(result)
+                temp_out += result
+            if count_newlines(temp_out) > 0 and not math.isinf(smallest_indent):
+                indent_str = strip_prefix(indentation * max(0, smallest_indent - 1), block_indent)
+                if i > 0 or is_wrapped:
+                    indent_str = block_indent + indent_str
+                temp_out = indent(dedent(temp_out), indent_str)
+                if i == 0:
+                    temp_out = strip_prefix(temp_out, block_indent)
+            out += temp_out
+        return remove_last_newline(out) if is_wrapped else out
 
     def eval_statement(stmt, env):
 
         nonlocal curr_indent
 
         if isinstance(stmt, TextStatement):
-            last_indent = 0
+            last_indent = ''
             has_newline = False
             for ch in reversed(stmt.text):
                 if ch == ' ':
-                    last_indent += 1
+                    last_indent += ' '
                 elif ch == '\n':
                     has_newline = True
                     break
                 else:
-                    last_indent = 0
+                    last_indent = ''
             if not has_newline:
                 if at_blank_line:
                     curr_indent = curr_indent + last_indent
@@ -193,20 +243,7 @@ def evaluate(ast, data={}, indentation='  '):
 
         elif isinstance(stmt, ForInStatement):
             rng = eval_code_expr(stmt.expression, env)
-            env2 = env.fork()
-            out = ''
-            for i in rng:
-                env2.set(stmt.pattern.name, i)
-                children = list(stmt.body)
-                strip_inner_before = len(children) > 0 and isinstance(children[0], TextStatement) and starts_with_newline(children[0].text)
-                #  strip_outer_after = len(children) > 0 and isinstance(children[-1], TextStatement) and ends_with_newline(children[-1].text)
-                smallest_indent = 0
-                for i, child in enumerate(children):
-                    result = eval_statement(child, env2)
-                    smallest_indent = min(smallest_indent, get_indentation(result))
-                    if i == 0 and strip_inner_before: result = remove_first_newline(result)
-                    out += result
-            return indent(dedent(out), indentation * smallest_indent)
+            return eval_repeat(stmt.pattern, rng, stmt.body)
 
         elif isinstance(stmt, ExpressionStatement):
             return str(eval_code_expr(stmt.expression, env))
@@ -214,21 +251,7 @@ def evaluate(ast, data={}, indentation='  '):
         elif isinstance(stmt, JoinStatement):
             rng = eval_code_expr(stmt.expression, env)
             sep = eval_code_expr(stmt.separator, env)
-            env2 = env.fork()
-            out = ''
-            for i in rng:
-                if i > 0: out += sep
-                env2.set(stmt.pattern.name, i)
-                children = list(stmt.body)
-                strip_inner_before = len(children) > 0 and isinstance(children[0], TextStatement) and starts_with_newline(children[0].text)
-                #  strip_outer_after = len(children) > 0 and isinstance(children[-1], TextStatement) and ends_with_newline(children[-1].text)
-                smallest_indent = 0
-                for i, child in enumerate(children):
-                    result = eval_statement(child, env2)
-                    smallest_indent = min(smallest_indent, get_indentation(result))
-                    if i == 0 and strip_inner_before: result = remove_first_newline(result)
-                    out += result
-            return indent(dedent(out), indentation * smallest_indent)
+            return eval_repeat(stmt.pattern, rng, stmt.body, sep)
 
         else:
             raise TypeError("Could not evaluate statement: unknown statement {}.".format(stmt))
